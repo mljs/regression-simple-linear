72e5f6148a8b4bdaf378272b724a05d65b01763c
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-01-07 14:24:04 +0100

    fix: update typescript definition (#3)
    * fix: update typescript definition
    * chore: use export assign instead of the default export

--- regression-simple-linear.d.ts
+++ regression-simple-linear.d.ts
@@ -1,22 +1,22 @@
 import BaseRegression from 'ml-regression-base';
 
-declare module 'ml-regression-simple-linear' {
+declare namespace SimpleLinearRegression {
   export interface SLRModel {
     name: 'simpleLinearRegression';
   }
-
-  class SimpleLinearRegression extends BaseRegression {
-    slope: number;
-    intercept: number;
-    coefficients: [number, number];
-
-    constructor(x: number[], y: number[]);
-
-    static load(model: SLRModel): SimpleLinearRegression;
-
-    computeX(y: number): number;
-    toJSON(): SLRModel;
-  }
-
-  export = SimpleLinearRegression;
 }
+
+declare class SimpleLinearRegression extends BaseRegression {
+  slope: number;
+  intercept: number;
+  coefficients: [number, number];
+
+  constructor(x: number[], y: number[]);
+
+  static load(model: SimpleLinearRegression.SLRModel): SimpleLinearRegression;
+
+  computeX(y: number): number;
+  toJSON(): SimpleLinearRegression.SLRModel;
+}
+
+export = SimpleLinearRegression;
