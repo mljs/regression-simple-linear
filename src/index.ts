3c8763f4740c6c52e901e1949d90ab0481bfccee
Santiago Miranda <santimir@protonmail.com>
2023-06-28 02:15:40 +0000

    typedoc: add workflow

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,9 @@ type JsonType = ReturnType<SimpleLinearRegression['toJSON']>;
 /**
  * Class representing simple linear regression.
  * The regression uses OLS to calculate intercept and slope.
- */
+ * @inheritDoc BaseRetgression
+*/
+
 export default class SimpleLinearRegression extends BaseRegression {
   slope!: number;
   intercept!: number;
