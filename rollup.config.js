55646c100b85ac6d842899454f03852b20af1e5d
Santiago Miranda <santimir@protonmail.com>
2023-07-03 10:53:22 +0100

    refactor: setup project for TypeScript (#8)

--- rollup.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'lib/index.js',
-    format: 'cjs',
-  },
-};
--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,41 @@ import BaseRegression, {
   maybeToPrecision,
 } from 'ml-regression-base';
 
+type JsonType = ReturnType<SimpleLinearRegression['toJSON']>;
+
+/**
+ * Class representing simple linear regression.
+ * The regression uses OLS to calculate intercept and slope.
+ */
+
 export default class SimpleLinearRegression extends BaseRegression {
-  constructor(x, y) {
+  slope!: number;
+  intercept!: number;
+  coefficients!: number[];
+
+  /**
+   * @param x - independent variable
+   * @param y - dependent variable
+   */
+  constructor(x: number[], y: number[]) {
     super();
+    // @ts-expect-error internal use of the constructor, from `this.load`
     if (x === true) {
-      this.slope = y.slope;
-      this.intercept = y.intercept;
-      this.coefficients = [y.intercept, y.slope];
+      // @ts-expect-error internal use of the constructor, from `this.load`
+      const yObj = y as JsonType;
+      this.slope = yObj.slope;
+      this.intercept = yObj.intercept;
+      this.coefficients = [yObj.intercept, yObj.slope];
     } else {
       checkArrayLength(x, y);
       regress(this, x, y);
     }
   }
 
+  /**
+   * Get the parameters and model name in JSON format
+   * @returns
+   */
   toJSON() {
     return {
       name: 'simpleLinearRegression',
@@ -24,15 +46,24 @@ export default class SimpleLinearRegression extends BaseRegression {
     };
   }
 
-  _predict(x) {
+  _predict(x: number): number {
     return this.slope * x + this.intercept;
   }
-
-  computeX(y) {
+  /**
+   * Finds x for the given y value.
+   * @param y - dependent variable value
+   * @returns - x value
+   */
+  computeX(y: number): number {
     return (y - this.intercept) / this.slope;
   }
 
-  toString(precision) {
+  /**
+   * Strings the linear function in the form 'f(x) = ax + b'
+   * @param precision - number of significant figures.
+   * @returns
+   */
+  toString(precision?: number): string {
     let result = 'f(x) = ';
     if (this.slope !== 0) {
       const xFactor = maybeToPrecision(this.slope, precision);
@@ -47,20 +78,30 @@ export default class SimpleLinearRegression extends BaseRegression {
     }
     return result;
   }
-
-  toLaTeX(precision) {
+  /**
+   * Strings the linear function in the form 'f(x) = ax + b'
+   * @param precision - number of significant figures.
+   * @returns
+   */
+  toLaTeX(precision?: number): string {
     return this.toString(precision);
   }
 
-  static load(json) {
+  /**
+   * Class instance from a JSON Object.
+   * @param json
+   * @returns
+   */
+  static load(json: JsonType): SimpleLinearRegression {
     if (json.name !== 'simpleLinearRegression') {
       throw new TypeError('not a SLR model');
     }
+    // @ts-expect-error internal use of the constructor
     return new SimpleLinearRegression(true, json);
   }
 }
 
-function regress(slr, x, y) {
+function regress(slr: SimpleLinearRegression, x: number[], y: number[]): void {
   const n = x.length;
   let xSum = 0;
   let ySum = 0;
